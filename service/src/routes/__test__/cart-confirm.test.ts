236123ca0bd1227c1cf9476d82caaf8a1511c5d8
tsendebna <tsendbayar.o@ebazaar.mn>
2024-09-26 14:38:10 +0800

    fix test

--- service/src/routes/__test__/cart-confirm.test.ts
+++ service/src/routes/__test__/cart-confirm.test.ts
@@ -1,9 +1,9 @@
 import request from "supertest";
 import { app } from "../../app";
 
-it("fails when a ID that does not exist is supplied", async () => {
+it("fails when a unauthorized", async () => {
   await request(app)
     .post(`${global.apiPrefix}/cart/confirm`)
     .send({})
-    .expect(400);
+    .expect(401);
 });
