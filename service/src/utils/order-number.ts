0ba8f59de7caee1ff2394402a509352ceabe9a8f
tsendebna <tsendbayar.o@ebazaar.mn>
2024-10-22 11:33:37 +0800

    order number seq

--- service/src/utils/order-number.ts
+++ service/src/utils/order-number.ts
@@ -3,17 +3,20 @@ import { Sequence } from "../shared/models/sequence";
 import moment from "moment";
 
 export const getOrderNumber = async (supplierId: string): Promise<any> => {
-    let code = "EB";
-    const duration = moment().format("YYMMDD");
-    const supplier = await Supplier.findById(supplierId);
-    if (supplier && supplier.code) {
-        code = supplier.code
-    }
+  let code = "EB";
+  const duration = moment().format("YYMMDD");
+  const supplier = await Supplier.findById(supplierId);
+  if (supplier && supplier.code) {
+    code = supplier.code;
+  }
 
-    let sequence = await Sequence.findOneAndUpdate({ code, duration }, { $inc: { seq: 1 } });
+  let sequence = await Sequence.findOneAndUpdate(
+    { code, duration },
+    { $inc: { seq: 1 } }
+  );
 
-    if (!sequence) {
-        sequence = await Sequence.create({ code, duration })
-    }
-    return `${code}${duration}${sequence.seq}`;
+  if (!sequence) {
+    sequence = await Sequence.create({ code, duration, seq: 1 });
+  }
+  return `${code}${duration}${sequence.seq}`;
 };
