fad494ef8f252fb8fc711a85946714ccaa26ef58
tsendebna <tsendbayar.o@ebazaar.mn>
2024-10-24 16:50:58 +0800

    order template list update

--- service/src/routes/template-get.ts
+++ service/src/routes/template-get.ts
@@ -15,8 +15,14 @@ const router = express.Router();
 router.get(
   "/template/get",
   [query("id").notEmpty().isString().withMessage("ID is required")],
-  [query("merchantId").notEmpty().isString().withMessage("Merchant ID is required")],
-  currentUser, requireAuth,
+  [
+    query("merchantId")
+      .notEmpty()
+      .isString()
+      .withMessage("Merchant ID is required"),
+  ],
+  currentUser,
+  requireAuth,
   validateRequest,
   async (req: Request, res: Response) => {
     const orderTemplate = await OrderTemplate.findById(req.query.id);
@@ -30,42 +36,53 @@ router.get(
 );
 
 const prepareTemplate = async (
-  template: OrderTemplateDoc, merchantId: Types.ObjectId
+  template: OrderTemplateDoc,
+  merchantId: Types.ObjectId
 ): Promise<any> => {
-  const promises = _.map(template.products, async (product, i) => {
-    await Inventory.find({ totalStock: 100 });
-    await Promo.findOne({});
-    try {
+  try {
+    const promises = _.map(template.products, async (product, i) => {
+      await Inventory.find({ totalStock: 100 });
+      await Promo.findOne({});
+      try {
+        const productPrice = await Product.findOneWithAdjustedPrice({
+          query: { _id: new Types.ObjectId(product.id) },
+          merchant: { merchantId: merchantId, businessTypeId: merchantId },
+        });
 
-      const productPrice = await Product.findOneWithAdjustedPrice({
-        query: { _id: new Types.ObjectId(product.id) },
-        merchant: { merchantId: merchantId, businessTypeId: merchantId },
-      });
+        const price = productPrice._adjustedPrice
+          ? productPrice._adjustedPrice.price + productPrice._adjustedPrice.cost
+          : 0;
 
-      const price = productPrice._adjustedPrice
-        ? productPrice._adjustedPrice.price + productPrice._adjustedPrice.cost
-        : 0;
-
-      return {
-        id: product.id,
-        name: productPrice.name,
-        images: productPrice.images,
-        description: productPrice.description,
-        quantity: product.quantity,
-        basePrice: price,
-        price,
-        giftQuantity: 0,
-        totalPrice: product.quantity * price,
-        stock: productPrice.inventory?.availableStock,
-        inCase: productPrice.inCase
-      };
-    } catch (error) {
-      return product
-    }
-  });
-  const products = await Promise.all(promises);
-  const merchant = await Customer.findById(merchantId);
-  const supplier = await Customer.findById(template.supplierId);
-  return { id: template.id, products, type: template.type, merchant: { id: merchant?.id, name: merchant?.name }, supplier: { id: supplier?.id, name: supplier?.name }, name: template.name, image: template.image, color: template.color }
+        return {
+          id: product.id,
+          name: productPrice.name,
+          images: productPrice.images,
+          description: productPrice.description,
+          quantity: product.quantity,
+          basePrice: price,
+          price,
+          giftQuantity: 0,
+          totalPrice: product.quantity * price,
+          stock: productPrice.inventory?.availableStock,
+          inCase: productPrice.inCase,
+        };
+      } catch (error) {
+        throw new Error("product not found");
+      }
+    });
+    const products = await Promise.all(promises);
+    const merchant = await Customer.findById(merchantId);
+    const supplier = await Customer.findById(template.supplierId);
+    return {
+      id: template.id,
+      products,
+      type: template.type,
+      merchant: { id: merchant?.id, name: merchant?.name },
+      supplier: { id: supplier?.id, name: supplier?.name },
+      name: template.name,
+      image: template.image,
+      color: template.color,
+    };
+  } catch (error) {}
 };
 export { router as templateGetRouter, prepareTemplate };
